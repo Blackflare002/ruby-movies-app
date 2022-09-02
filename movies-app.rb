2efcd67240d3e8f983dc30d776ea573b2588e38e
Shane Phillips <shane@loongphillips.com>
2022-09-02 15:49:54 -0400

    add Prettier for Ruby, gitignore file

--- movies-app.rb
+++ movies-app.rb
@@ -1,45 +1,39 @@
-movies = {
-    Stealth: 4,
-    Terminator: 5,
-    Alien: 5,
-    Totoro: 4,
-  }
-  puts "What would you like to do?"
-  choice = gets.chomp
-  case choice
-    when "add"
-      puts "What's the movie's title?"
-      title = gets.chomp.to_sym
-      if movies[title.to_sym].nil?
-        puts "How would you rate that movie on a scale of 0 to 5?"
-        rating = gets.chomp.to_i
-        movies[title.to_sym] = rating.to_i
-        puts "Movie added!"
-      else
-        puts "Movie has already been added!"
-      end    
-    when "update"
-      puts "What's the movie's title?"
-      title = gets.chomp
-      if movies[title.to_sym].nil?
-        puts "You've never added this movie!"
-      else
-        puts "What's the movie's new rating?"
-        rating = gets.chomp
-        movies[title.to_sym] = rating.to_i
-      end
-    when "display"
-      movies.each do |movie, rating|
-        puts "#{movie}: #{rating}"
-      end
-    when "delete"
-      puts "What's the movie's title?"
-      title = gets.chomp
-      if movies[title.to_sym].nil?
-        puts "You've never added this movie!"
-      else
-        movies.delete(title.to_sym)
-      end
-    else
-      puts "Error!"
+movies = { Stealth: 4, Terminator: 5, Alien: 5, Totoro: 4 }
+
+puts "What would you like to do?"
+choice = gets.chomp
+case choice
+when "add"
+  puts "What's the movie's title?"
+  title = gets.chomp.to_sym
+  if movies[title.to_sym].nil?
+    puts "How would you rate that movie on a scale of 0 to 5?"
+    rating = gets.chomp.to_i
+    movies[title.to_sym] = rating.to_i
+    puts "Movie added!"
+  else
+    puts "Movie has already been added!"
   end
+when "update"
+  puts "What's the movie's title?"
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "You've never added this movie!"
+  else
+    puts "What's the movie's new rating?"
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+  end
+when "display"
+  movies.each { |movie, rating| puts "#{movie}: #{rating}" }
+when "delete"
+  puts "What's the movie's title?"
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "You've never added this movie!"
+  else
+    movies.delete(title.to_sym)
+  end
+else
+  puts "Error!"
+end
